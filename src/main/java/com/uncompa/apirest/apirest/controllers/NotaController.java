09aa2460817edc719495ca694f8bcc1f73028689
UnCompa <brandonddxd@gmail.com>
2024-08-18 14:07:16 -0500

    Cors agregado a post, creo

--- src/main/java/com/uncompa/apirest/apirest/controllers/NotaController.java
+++ src/main/java/com/uncompa/apirest/apirest/controllers/NotaController.java
@@ -18,6 +18,7 @@ public class NotaController {
     private NotaService notaService;
 
     // Crear una nueva nota
+    @CrossOrigin(origins = "http://localhost:4321")
     @PostMapping
     public Nota crearNota(@RequestBody Nota nota) {
         return notaService.crearNota(nota);
